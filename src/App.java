35a29326ce40e01e2084f162182ed74bc1febdf7
anawats <prapugkonk67@nu.ac.th>
2026-02-06 15:36:25 +0700

    change a2==>app

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        WaterApp a2 = new WaterApp();
-        a2.setVisible(true);
+        WaterApp app = new WaterApp();
+        app.setVisible(true);
     }
 }
